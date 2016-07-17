451336702207e01d066a0952bfd90cdcab396605
Brendon Boshell <brendonboshell@gmail.com>
2016-07-17 11:16:58 +0100

    Crawler to use FifoUrlList by default

--- test/Crawler.spec.js
+++ test/Crawler.spec.js
@@ -1,8 +1,24 @@
-var Crawler = require("../lib/Crawler"),
-    expect = require("chai").expect;
+var proxyquire = require('proxyquire'),
+    expect = require("chai").expect,
+    Crawler,
+    FifoUrlListMock;
+
+FifoUrlListMock = function () {
+
+};
+
+Crawler = proxyquire("../lib/Crawler", {
+  "./FifoUrlList": FifoUrlListMock
+});
 
 describe("Crawler", function () {
   it("returns an instance when called as a function", function () {
     expect(Crawler()).to.be.an.instanceOf(Crawler);
+  });
+
+  it("if no urlList is specified, defaults to a FifoUrlList", function () {
+    var crawler = new Crawler();
+
+    expect(crawler.getUrlList()).to.be.an.instanceOf(FifoUrlListMock);
   });
 });
