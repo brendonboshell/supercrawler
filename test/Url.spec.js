cebe5a23ef3d02bf2066c3350dcdf0e40b8fbb29
Jumpei Ogawa <phanective@gmail.com>
2019-09-22 19:39:10 +0900

    Add trailing slashes to the URLs of the root pages in tests

--- test/Url.spec.js
+++ test/Url.spec.js
@@ -4,31 +4,31 @@ var Url = require("../lib/Url"),
 describe("Url", function () {
   it("returns an instance when called as a function", function () {
     expect(Url({
-      url: "https://example.com"
+      url: "https://example.com/"
     })).to.be.an.instanceOf(Url);
   });
 
   it("uses URL as the unique ID", function () {
     expect(new Url({
-      url: "https://example.com"
-    }).getUniqueId()).to.equal("https://example.com");
+      url: "https://example.com/"
+    }).getUniqueId()).to.equal("https://example.com/"); 
   });
 
   it("accepts a string URL as the only argument", function () {
-    expect(new Url("https://example.com").getUniqueId()).to.equal("https://example.com");
+    expect(new Url("https://example.com/").getUniqueId()).to.equal("https://example.com/");
   });
 
   describe("#getStatusCode", function () {
     it("returns the statusCode if specified", function () {
       expect(new Url({
-        url: "https://example.com",
+        url: "https://example.com/",
         statusCode: 201
       }).getStatusCode()).to.equal(201);
     });
 
     it("defaults to null if statusCode not specified", function () {
       expect(new Url({
-        url: "https://example.com"
+        url: "https://example.com/"
       }).getStatusCode()).to.equal(null);
     });
   });
@@ -36,15 +36,15 @@ describe("Url", function () {
   describe("#getUrl", function () {
     it("returns the URL", function () {
       expect(new Url({
-        url: "https://example.com"
-      }).getUrl()).to.equal("https://example.com");
+        url: "https://example.com/"
+      }).getUrl()).to.equal("https://example.com/");
     });
   });
 
   describe("#getErrorCode", function () {
     it("returns the error code", function () {
       expect(new Url({
-        url: "https://example.com",
+        url: "https://example.com/",
         errorCode: "ROBOTS_NOT_ALLOWED"
       }).getErrorCode()).to.equal("ROBOTS_NOT_ALLOWED");
     });
@@ -53,7 +53,7 @@ describe("Url", function () {
   describe("#getErrorMessage", function () {
     it("returns the error message", function () {
       expect(new Url({
-        url: "https://example.com",
+        url: "https://example.com/",
         errorCode: "ROBOTS_NOT_ALLOWED",
         errorMessage: "A robots.txt error"
       }).getErrorMessage()).to.equal("A robots.txt error");
@@ -61,7 +61,7 @@ describe("Url", function () {
 
     it("returns null if no rror message", function () {
       expect(new Url({
-        url: "https://example.com",
+        url: "https://example.com/",
         errorCode: "ROBOTS_NOT_ALLOWED"
       }).getErrorMessage()).to.equal(null);
     });
